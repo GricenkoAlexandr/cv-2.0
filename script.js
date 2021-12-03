5016b37fc9845c8ba87fe62716efc1784365f2b6
Alexandr <alexgriko29@gmail.com>
2021-12-03 08:07:24 +0200

    add slider

--- script.js
+++ script.js
@@ -2,17 +2,24 @@
 console.log('JS подключен!');
 
 const btnl=document.querySelector('.lftbtn');
+const btnr=document.querySelector('.rtbtn');
+
 let avas=document.querySelectorAll('.avatar');
+let point=0;
+moveAva();
 
 btnl.addEventListener('click', moveAva);
 
 function moveAva() {
-    console.log('left');
-    console.log(avas);
-    avas.forEach(item=> {
-        if (item.style.classList==='hidden') {
-            console.log(item);
-        }
+    for (let i=0; i<avas.length; i++) {
+        avas[i].classList.add('hidden');
     }
-    )
+    avas[point].classList.remove('hidden');
+    if (point+1==avas.length) {
+        point=0;
+    } else {
+        point++
+    }
 }
+
+btnl.addEventListener('click', moveAva);
