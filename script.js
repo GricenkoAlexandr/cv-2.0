b964dd1233f72629722781a99f2d791fa5dc5fd9
Alexandr <alexgriko29@gmail.com>
2021-12-03 22:50:12 +0200

    add new photo

--- script.js
+++ script.js
@@ -3,23 +3,35 @@ console.log('JS подключен!');
 
 const btnl=document.querySelector('.lftbtn');
 const btnr=document.querySelector('.rtbtn');
-
 let avas=document.querySelectorAll('.avatar');
 let point=0;
-moveAva();
 
-btnl.addEventListener('click', moveAva);
+btnl.addEventListener('click', leftmove);
+btnr.addEventListener('click', rightmove);
+
+moveAva();
 
 function moveAva() {
     for (let i=0; i<avas.length; i++) {
-        avas[i].classList.add('hidden');
+        avas[i].classList.add('hidden')
     }
     avas[point].classList.remove('hidden');
+}
+
+function leftmove() {
     if (point+1==avas.length) {
         point=0;
     } else {
-        point++
+        point++;
     }
+    moveAva();
 }
 
-btnl.addEventListener('click', moveAva);
+function rightmove() {
+    if (point-1==-1) {
+        point=avas.length-1;
+    } else {
+        point--;
+    }
+    moveAva();
+}
