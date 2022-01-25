2108419ce7b79be36f09924d9d67fd25aa2bec9e
Alexandr <alexgriko29@gmail.com>
2022-01-25 23:22:50 +0200

    add new audio

--- script.js
+++ script.js
@@ -69,6 +69,20 @@ function togglefunc() {
 
 /* Company accordeon end */
 
+/* Modal */
+
+let modBtn=document.querySelector('.btn_in');
+let modWin=document.querySelector('.modal_win');
+
+function modalhide() {
+    modWin.classList.add('hidden');
+}
+
+modBtn.addEventListener('click', modalhide)
+
+/* Modal end */
+
+
 /* Audio */
 
 let aud=document.querySelector('.audioset');
@@ -84,23 +98,20 @@ muzon();
 let musoff=document.querySelector('.soundbtn');
 
 musoff.addEventListener("click", ()=> {
-  aud.volume=0.0;
-  console.log('Музика выкл')
+    
     musoff.classList.toggle('muzonoff');
-    musoff.innerHTML='<i class="fas fa-volume-mute"></i>';
+
+    if (musoff.classList.contains('muzonoff')) {
+        musoff.innerHTML='<i class="fas fa-volume-mute"></i>';
+        aud.volume=0.0;
+        console.log('Музика выкл')
+
+    } else {
+        musoff.innerHTML='<i class="fas fa-volume-off"></i>';
+        aud.volume=0.1;
+        console.log('Музика вкл')
+    }
+    
 })
 
 /* Audio end */
-
-/* Modal */
-
-let modBtn=document.querySelector('.btn_in');
-let modWin=document.querySelector('.modal_win');
-
-function modalhide() {
-    modWin.classList.add('hidden');
-}
-
-modBtn.addEventListener('click', modalhide)
-
-/* Modal end */
